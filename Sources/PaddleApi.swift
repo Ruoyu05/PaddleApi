a6b2aac1183bf3050dacbc95f8ec4537e4910085
Ruoyu05 <xyhuyumao@gmail.com>
2025-08-26 22:05:19 +0900

    update podspec

---
+++ Sources/PaddleApi.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+public class PaddleApi {
+
+    public init() throws {
+       
+    }
+
+    public func downloadModel(url: URL, fileName: String, completion: @escaping (Result<URL, Error>) -> Void) {
+       
+    }
+}
